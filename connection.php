d3d26df5ebe68b7324bdfaed585f234fee334e4c
Serena490 <serenagasper490@gmail.com>
2021-11-29 12:07:35 +0000

    ADD DATABASE CONNECTION AND HOMEPAGE TOGETHER

--- connection.php
+++ connection.php
@@ -1,3 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title> Space-range homepage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Space-Range Homepage</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div class="d-grid gap-6">
+  <button type="button" class="btn btn-dark btn-block" onclick="window.location.href='Mission_Form.php'">Create Mission</button>
+  <button type="button" class="btn btn-dark btn-block" onclick="window.location.href='https://w3docs.com'">View Mission</button>
+  <button type="button" class="btn btn-dark btn-block" onclick="window.location.href='https://w3docs.com'" >Amend Mission</button>
+   <button type="button" class="btn btn-dark btn-block" onclick="window.location.href='astronaut_form.php'">Create Astronaut ID</button>
+     <button type="button" class="btn btn-dark btn-block" onclick="window.location.href='https://w3docs.com'">View Astronaut ID </button>
+       <button type="button" class="btn btn-dark btn-block" onclick="window.location.href='https://w3docs.com'">Amend Astronaut ID</button>
+</div>
 <?php
 $servername = "localhost";
 $username = "admin";
@@ -12,3 +38,5 @@ try {
   echo "Connection failed: " . $e->getMessage();
 }
 ?>
+</body>
+</html>
