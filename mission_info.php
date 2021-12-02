a3c0a7c74ce41c4e2022578e5970d7dcbc48d2be
Serena490 <serenagasper490@gmail.com>
2021-12-02 11:16:11 +0000

    a

--- mission_info.php
+++ mission_info.php
@@ -1,10 +1,15 @@
 <?php
-
 $servername = "localhost";
-$username = "admin";
+$username = "username";
 $password = "password";
+$dbname = "Table_Homepage";
 
-$sql = "INSERT INTO mission (name, destination, launch_date, type, crew_size, target_id)
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+$sql = "INSERT INTO Mission (name, destination, launch_date, type, crew_size, target_id)
 VALUES ('".$_POST["name"]."','".$_POST["destination"]."','".$_POST["launch_date"]."'$_POST["type"]."'$_POST["crew_size"]."'$_POST["target_id"])";
 if ($dbh->query($sql)) {
 echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
@@ -12,15 +17,5 @@ echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');
 else{
 echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
 }
-
-$dbh = null;
-}
-catch(PDOException $e)
-{
-echo $e->getMessage();
-}
-
-}
-
-
-<
+$conn = null;
+?>
