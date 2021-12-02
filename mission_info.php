0af76b2698eec255062a7b58fdcc3bc026431a9f
Serena490 <serenagasper490@gmail.com>
2021-12-02 09:30:29 +0000

    e

--- mission_info.php
+++ mission_info.php
@@ -1,16 +1,4 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=Table_Homepage", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
- 
-// Attempt insert query execution
 try{
     $sql = "INSERT INTO mission (target_id, destination, type, crew_size, name, launch_date) VALUES ('','', '','','','')";    
     $pdo->exec($sql);
