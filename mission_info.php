cd86b67427020a841e3ff9b159240c4334d2339c
Serena490 <serenagasper490@gmail.com>
2021-12-02 09:21:53 +0000

    w

--- mission_info.php
+++ mission_info.php
@@ -1,53 +1,24 @@
-<html>
-<head>
-<title>insert data in database using PDO(php data object)</title>
-
-</head>
-<body>
-
-<div id="main">
-<h1>Insert data into database using PDO</h1>
-<div id="login">
-<h2>Student's Form</h2>
-<hr/>
-<form action="" method="post">
-<label>Student Name :</label>
-<input type="text" name="stu_name" id="name" required="required" placeholder="Please Enter Name"/><br /><br />
-<label>Student Email :</label>
-<input type="email" name="stu_email" id="email" required="required" placeholder="john123@gmail.com"/><br/><br />
-<label>Student City :</label>
-<input type="text" name="stu_city" id="city" required="required" placeholder="Please Enter Your City"/><br/><br />
-<input type="submit" value=" Submit " name="submit"/><br />
-</form>
-</div>
-
 <?php
-if(isset($_POST["submit"])){
-$hostname='localhost';
-$username='admin';
-$password='password';
-
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
 try {
-$dbh = new PDO("mysql:host=$hostname;dbname=Table_Homepage",$username,$password);
-
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // <== add this line
-$sql = "INSERT INTO mission (student_name, student_email, student_city)
-VALUES ('".$_POST["stu_name"]."','".$_POST["stu_email"]."','".$_POST["stu_city"]."')";
-if ($dbh->query($sql)) {
-echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+    $conn = new PDO("mysql:host=$servername;dbname=Table_Homepage", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+ 
+// Attempt insert query execution
+try{
+    $sql = "INSERT INTO mission (target_id, destination, type, crew_size, name, launch_date) VALUES ('Peter', 'Parker', 'peterparker@mail.com','','','')";    
+    $pdo->exec($sql);
+    echo "Records inserted successfully.";
+} catch(PDOException $e){
+    die("ERROR: Could not able to execute $sql. " . $e->getMessage());
 }
-else{
-echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
-}
-
-$dbh = null;
-}
-catch(PDOException $e)
-{
-echo $e->getMessage();
-}
-
-}
+ 
+// Close connection
+unset($pdo);
 ?>
-</body>
-</html>
