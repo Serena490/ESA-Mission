e9a5adb89c5673c12019252f4b17337f221ac807
Serena490 <serenagasper490@gmail.com>
2021-12-06 09:35:42 +0000

    test 100

--- updated file trial/mission_forms.php
+++ updated file trial/mission_forms.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
@@ -34,5 +34,61 @@
     <input type="submit" value="submit">
   
 </form>
+</body>
+</html> 
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Contact Form</h2>
+                    </div>
+                    <p>Please fill this form and submit to add employee record to the database.</p>
+                    <form action="insert.php" method="post">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Destination</label>
+                            <input type="text" name="destination" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Launch Date</label>
+                            <input type="text" name="launch_date" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Type</label>
+                            <input type="text" name="type" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Crew Size</label>
+                            <input type="number" name="crew_size" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Target ID</label>
+                            <input type="number" name="target_id" class="form-control">
+                        </div>
+                        <input type="submit" class="btn btn-primary" name="submit" value="Submit">
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
 </body>
 </html>
--- updated file trial/mission_infos.php
+++ updated file trial/mission_infos.php
@@ -1,43 +1,29 @@
 <?php
-$servername = "localhost";
-$username = "admin";
-$password = "password";
+    $servername='localhost';
+    $username='admin';
+    $password='password';
+    $dbname = "Table_Homepage";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+      if(!$conn){
+          die('Could not Connect MySql Server:' .mysql_error());
+        }
+include_once 'mission_forms.php';
+if(isset($_POST['submit']))
+{    
+     $name = $_POST['name'];
+     $destination = $_POST['destination'];
+     $type = $_POST['type'];
+     $target_id = $_POST['target_id];
+     $launch_date = $_POST['launch_date'];
+     $crew_size = $_POST['crew_size'];
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=Table_Homepage", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+     $sql = "INSERT INTO Mission (name,destination,type,target_id,crew_size,launch_date)
+     VALUES ('$name','$destination','$type','$target_id','$crew_size','$launch_date')";
+     if (mysqli_query($conn, $sql)) {
+        echo "New record has been added successfully !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+     }
+     mysqli_close($conn);
 }
-
-
-   // Taking all 5 values from the form data(input)
-   $target_id =  $_REQUEST['target_id'];
-   $destination = $_REQUEST['destination'];
-   $type =  $_REQUEST['type'];
-   $crew_size = $_REQUEST['crew_size'];
-   $name = $_REQUEST['name'];
-   $launch_date = $_REQUEST['launch_date'];
-   
-     
-   // Performing insert query execution
-   // here our table name is college
-   $sql = "INSERT INTO Mission  VALUES ('$target_id', 
-       '$destination','$type','$crew_size','$name','$launch_date)";
-     
-   if(mysqli_query($conn, $sql)){
-       echo "<h3>data stored in a database successfully." 
-           . " Please browse your localhost php my admin" 
-           . " to view the updated data</h3>"; 
-
-       echo nl2br("\n$target_id\n $destination\n "
-           . "$type\n $crew_size\n $name \n $launch__date");
-   } else{
-       echo "ERROR: Hush! Sorry $sql. " 
-           . mysqli_error($conn);
-   }
-     
-   // Close connection
 ?>
