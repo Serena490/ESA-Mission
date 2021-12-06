7ea812323b137535f24d5a7f4acd8684d879ca04
Serena490 <serenagasper490@gmail.com>
2021-12-06 17:41:43 +0000

    f

--- updated file trial/mission_infos.php
+++ updated file trial/mission_infos.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
     $servername='localhost';
     $username='admin';
     $password='password';
@@ -20,6 +20,34 @@ if(isset($_POST['submit']))
      $sql =  "SET FOREIGN_KEY_CHECKS = 0;";
      $sql = "INSERT INTO Mission (name,destination,type,crew_size,launch_date,target_id)
      VALUES ('$name','$destination','$type','$crew_size','$launch_date','$target_id')";
+     
+
+     if (mysqli_query($conn, $sql)) {
+        echo "New record has been added successfully !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+     }
+     mysqli_close($conn);
+}
+?> */
+<?php
+    $servername='localhost';
+    $username='admin';
+    $password='password';
+    $dbname = "test";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+      if(!$conn){
+          die('Could not Connect MySql Server:' .mysql_error());
+        }
+include_once 'testingform.php';
+if(isset($_POST['submit']))
+{    
+     $name = $_POST['name'];
+  
+    
+     $sql =  "SET FOREIGN_KEY_CHECKS = 0;";
+     $sql = "INSERT INTO table1 (name)
+     VALUES ('$name')";
      
 
      if (mysqli_query($conn, $sql)) {
