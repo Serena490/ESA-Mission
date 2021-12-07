95d81c5876b4194736284a37b1ac9d2f8d46392a
Serena490 <serenagasper490@gmail.com>
2021-12-07 09:10:43 +0000

    a

--- updated file trial/target_infos.php
+++ updated file trial/target_infos.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
     $servername='localhost';
     $username='admin';
     $password='password';
@@ -20,6 +20,33 @@ if(isset($_POST['submit']))
      VALUES ('$name','$first_mission','$type','$no_missions',)";
      
      
+     if (mysqli_query($conn, $sql)) {
+        echo "New record has been added successfully !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+     }
+     mysqli_close($conn);
+}
+?> */
+    $servername='localhost';
+    $username='admin';
+    $password='password';
+    $dbname = "test";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+      if(!$conn){
+          die('Could not Connect MySql Server:' .mysql_error());
+        }
+include_once 'testingform.php';
+if(isset($_POST['submit']))
+{    
+     $name = $_POST['name'];
+  
+    
+     $sql =  "SET FOREIGN_KEY_CHECKS = 0;";
+     $sql = "INSERT INTO test2 (name)
+     VALUES ('$name')";
+     
+
      if (mysqli_query($conn, $sql)) {
         echo "New record has been added successfully !";
      } else {
