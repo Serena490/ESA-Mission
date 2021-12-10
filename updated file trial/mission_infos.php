7323b010feb364d7f730e15b484880412e5bf335
Serena490 <serenagasper490@gmail.com>
2021-12-10 11:12:47 +0000

    w

--- updated file trial/mission_infos.php
+++ updated file trial/mission_infos.php
@@ -51,14 +51,13 @@ if(isset($_POST['submit']))
      $target_id = $conn->insert_id;
      $destination = $_POST['destination'];
      $type = $_POST['type'];
-     $launch_date = $_POST['launch_date'];
      $crew_size = $_POST['crew_size'];
      $launch_date = $_POST['launch_date'];
      $mission_name = $_POST['mission_name'];
 // This tells the php file that each variable will equal the information which has been sent from the mission_forms.php file which is the user input inside the form
      $sql =  "SET FOREIGN_KEY_CHECKS = 0;";
-     $sql = "INSERT INTO Mission (name,destination,type,crew_size,launch_date,target_id)
-     VALUES ('$name','$destination','$type','$crew_size','$launch_date','$target_id')";
+     $sql = "INSERT INTO Mission (target_id,destination,type,crew_size,mission_name,launch_date)
+     VALUES ('$target_id','$destination','$type','$crew_size','$mission_name','$launch_date')";
 // The $sql attribute allows the information which has been inputted to the user to be sent to the database and translatted from php to SQL so that phpMyAdmin understands what it is being told to do 
 // When this is executed the information inputted by the user is saved under the variables and will be sent to the database and is inserted into the specified column names and table
      if (mysqli_query($conn, $sql)) {
