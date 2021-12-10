2dc1aed53b5985a168b64b24248a8d9bf9d496cb
Serena490 <serenagasper490@gmail.com>
2021-12-10 10:05:22 +0000

    s

--- updated file trial/mission_infos.php
+++ updated file trial/mission_infos.php
@@ -2,7 +2,7 @@
     $servername='localhost';
     $username='admin';
     $password='password';
-    $dbname = "Table_Homepage";
+    $dbname = "ESA";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
@@ -12,7 +12,7 @@ include_once 'mission_forms.php';
 // This tells the code to link mission_forms.php to this php file
 if(isset($_POST['submit']))
 {    
-     $name = $_POST['name'];
+     $name = $_POST['mission_name'];
      $destination = $_POST['destination'];
      $launch_date = $_POST['launch_date'];
      $crew_size = $_POST['crew_size'];
@@ -20,7 +20,7 @@ if(isset($_POST['submit']))
      $target_id = $conn->insert_id;
 // This tells the php file that each variable will equal the information which has been sent from the mission_forms.php file which is the user input inside the form
      $sql =  "SET FOREIGN_KEY_CHECKS = 0;";
-     $sql = "INSERT INTO Mission (name,destination,type,crew_size,launch_date,target_id)
+     $sql = "INSERT INTO Mission (mission_name,destination,type,crew_size,launch_date,target_id)
      VALUES ('$name','$destination','$type','$crew_size','$launch_date','$target_id')";
 // The $sql attribute allows the information which has been inputted to the user to be sent to the database and translatted from php to SQL so that phpMyAdmin understands what it is being told to do 
 // When this is executed the information inputted by the user is saved under the variables and will be sent to the database and is inserted into the specified column names and table
--- updated file trial/mission_view.php
+++ updated file trial/mission_view.php
@@ -38,7 +38,7 @@ tr:nth-child(even) {
 $servername='localhost';
 $username='admin';
 $password='password';
-$dbname = "Table_Homepage";
+$dbname = "ESA";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
     die('Could not Connect MySql Server:' .mysql_error());
