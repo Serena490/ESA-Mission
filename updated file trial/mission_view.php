62ec304605394f58a447f528f8bfd1771ec5ef4f
Serena490 <serenagasper490@gmail.com>
2021-12-10 09:48:54 +0000

    f

--- updated file trial/mission_view.php
+++ updated file trial/mission_view.php
@@ -10,8 +10,7 @@ if(!$conn){
 
 $sql =  "SELECT * FROM Mission";
 $result = mysqli_query($conn, $sql);
-$result = mysqli_fetch_assoc($result);
-echo $result['target_id','destination','type','crew_size','mission_name','launch_date'];
-
-
+foreach ($result as $row) {
+    echo $row['target_id','destination','type','crew_size','mission_name','launch_date'];
+}
 ?> 
